fe04684c41222a1c44b937163916e78a98910900
Fe-56 <limfuoen1999@gmail.com>
2023-01-13 23:03:12 -0800

    Set up components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Hobbies from "./components/Hobbies";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar /> */}
+      <About />
+      {/* <Projects /> */}
+      {/* <Skills /> */}
+      {/* <Experience /> */}
+      {/* <Contact /> */}
+      {/* <Hobbies /> */}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Experience.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Experience() {
+  return (
+    <div id="experience">
+      
+    </div>
+  );
+}
