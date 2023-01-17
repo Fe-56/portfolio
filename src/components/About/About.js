d9a1c16fcf6a4e6c8b42f287f0080a503071a7c2
Fe-56 <limfuoen1999@gmail.com>
2023-01-16 23:18:57 -0800

    Update About component
    Updated the URL for profile picture and added an about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,22 +5,23 @@ import * as d3 from 'd3';
 const name = "Fuo En Lim";
 const title = "Aspiring Full-Stack Developer";
 const quote = "I love to build cool apps that bring value to the lives of others!";
-const profilePictureURL = "https://sutdapac-my.sharepoint.com/personal/fuoen_lim_mymail_sutd_edu_sg/Documents/University%20stuff/Internships/May%20to%20Aug%202023/profile_picture_portfolio.jpg";
+const profilePictureURL = "https://drive.google.com/uc?export=view&id=1IBlX714h5NUGucyQeUrr8JUsvGwlZ6me";
+const about = "Fuo En was born in Singapore in 1999 as a third-generation Singapore Citizen. Since young, he has an interest in computers and technology, and aspired to have a career that involved technology. Through the years, his aspirations have manifested into a passion to solve problems using technology and applying design and entrepreneurial thinking. He is currently a Junior at Singapore University of Technology and Design (SUTD), majoring in Computer Science and Design (CSD), and a visiting student at UC Berkeley Extension, studying for the Professional Certificate in Entrepreneurship until May 2023.";
 const milestoneCircleRadius = 10;
 const distanceBetweenMilestoneAndTimeline = 50;
 const milestones = [
   {2016: "First venture into the world of coding. Attended a basic Android app development course using storyboards at Nanyang Technological University (NTU)."},
   {2017: "Attended a coding course at Singapore Management University (SMU), where I learnt Ruby, the first programming language I've ever learnt."},
-  {2020: "Matriculated at Singapore University of Technology and Design (SUTD)."},
+  {2020: "Matriculated at SUTD."},
   {2021: "Completed my first ever internship at DBS Bank as a Business Analyst Intern."},
   {2022: "Completed mobile application development internship at Housing & Development Board (HDB)."},
-  {2023: "Studying for the Professional Certificate in Entrepreneurship at UC Berkeley Extension."}
+  {2023: "Studying at UC Berkeley."}
 ];
 
 export default function About() {
   useEffect(()=> {
-    let width = 1000, height = 650, padding = 160;
-    let svg = d3.select("#bio")
+    let width = 900, height = 650, padding = 160;
+    let svg = d3.select("#timeline")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height)
@@ -105,59 +106,60 @@ export default function About() {
   });
 
   return (
-    <div id="about">
-      <h1 id="name">{name}</h1>
-      <img src={profilePictureURL} alt="Lim Fuo En profile picture" id="profile_picture"></img>
-      <h2 id="title">{title}</h2>
-      <div class="d-flex justify-content-center align-items-center">
-        <blockquote>
-          <p id="quote">{quote}</p>
-        </blockquote>
+    <div id="about" class="justify-content-center align-items-center container-fluid">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h1 id="name">{name}</h1>
+            <h5 id="title">{title}</h5>
+          </div>
+          <div class="col-lg-6">
+            <img src={profilePictureURL} alt="Lim Fuo En profile picture" id="profile_picture"></img>
+          </div>
+        </div>
+      </div>
+      <blockquote>
+        <p id="quote">{quote}</p>
+      </blockquote>
+      <div class="spacer"></div>
+      <div id="about">
+        <h3><u>About</u></h3>
+        <p>{about}</p>
       </div>
       <div class="spacer"></div>
-      <div class="justify-content-center align-items-center">
-        <div id="bio">
-          <h3><u>(Professional) Bio</u></h3>
-        </div>
-        <div class="spacer"></div>
-        <div id="hobbies">
-          <h3><u>Hobbies</u></h3>
-          <p id="hobbies_text">Road Cycling, Jamming on the Electric Guitar, Watching Anime, Watching Netflix, Building Cool Stuff with Code</p>
-        </div>
-        <div class="spacer"></div>
-        <div id="find_me">
-          <h3><u>Find me on the Web</u></h3>
-          <div class="container">
-            <div class="row">
-              <div class="col-sm">
-                <div class="find_me_button_container">
-                  <a href="https://github.com/Fe-56" target="_blank" class="find_me_button">
-                    <i class="fa-brands fa-github fa-2xl"></i>
-                    <p>Github</p>
-                  </a>
-                </div>
+      <div id="timeline">
+        <h3><u>(Professional) Timeline</u></h3>
+      </div>
+      <div class="spacer"></div>
+      <div id="hobbies">
+        <h3><u>Hobbies</u></h3>
+        <p id="hobbies_text">Road Cycling, Jamming on the Electric Guitar, Watching Anime, Watching Netflix, Building Cool Stuff with Code</p>
+      </div>
+      <div class="spacer"></div>
+      <div id="find_me">
+        <h3><u>Find me on the Web</u></h3>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm">
+              <div class="find_me_button_container">
+                <a href="https://github.com/Fe-56" target="_blank" class="find_me_button">
+                  <i class="fa-brands fa-github fa-2xl"></i>
+                  <p>Github</p>
+                </a>
               </div>
-              <div class="col-sm">
-                <div class="find_me_button_container">
-                  <a href="https://www.linkedin.com/in/limfuoen/" target="_blank" class="find_me_button">
-                    <i class="fa-brands fa-linkedin fa-2xl"></i>
-                    <p>LinkedIn</p>
-                  </a>
-                </div>
-              </div>
-              <div class="col-sm">
-                <div class="find_me_button_container">
-                  <a href="https://www.instagram.com/fuoen/" target="_blank" class="find_me_button">
-                    <i class="fa-brands fa-instagram fa-2xl"></i>
-                    <p>Instagram</p>
-                  </a>
-                </div>
+            </div>
+            <div class="col-sm">
+              <div class="find_me_button_container">
+                <a href="https://www.linkedin.com/in/limfuoen/" target="_blank" class="find_me_button">
+                  <i class="fa-brands fa-linkedin fa-2xl"></i>
+                  <p>LinkedIn</p>
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div> 
+    </div>
   );
 }
 
