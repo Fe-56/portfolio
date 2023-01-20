6a57d38092dd0e8106ea9c062db1c67205bca2bd
Fe-56 <limfuoen1999@gmail.com>
2023-01-20 11:36:30 -0800

    Navbar ready

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,37 +4,44 @@ import './Navbar.css';
 export default function Navbar() {
   return ( 
     <div id="navbar">
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top justify-content-center" id="navbar_nav">
-        <div class="container-fluid">
-          <a href="/portfolio" class="navbar-brand mb-0">
-            <img src="http://fe-56.github.io/portfolio/favicon.ico?v=2" width="30" height="30" class="d-inline-block align-top"/>
-            Fuo En Lim
-          </a>
-          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarButtons" aria-controls="navbar_buttons" aria-expanded="false" aria-label="Toggle navigation bar">
+      <nav class="navbar navbar-expand-sm navbar-dark fixed-top" id="navbar_nav">
+        <div class="container-fluid justify-content-center">
+          <div id="navbar_logo">
+            <a href="/portfolio" class="navbar-brand mb-0 mx-auto" id="logo">
+              <img src="http://fe-56.github.io/portfolio/favicon.ico?v=2" width="30" height="30" class="d-inline-block align-top" id="fe56_logo"/>
+              Fuo En Lim
+            </a>
+          </div>
+          <div class="horizontal_gap"></div>
+          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar_buttons" aria-controls="navbar_buttons" aria-expanded="false" aria-label="Toggle navigation bar">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarButtons">
+          <div class="collapse navbar-collapse justify-content-center" id="navbar_buttons">
             <ul class="navbar-nav">
               <li class="nav-item active">
                 <a href="/portfolio/projects" class="nav-link active">
                   Projects
                 </a>
               </li>
+              <div class="horizontal_gap"></div>
               <li class="nav-item active">
                 <a href="/portfolio/skills" class="nav-link active">
                   Skills
                 </a>
               </li>
+              <div class="horizontal_gap"></div>
               <li class="nav-item active">
                 <a href="/portfolio/experience" class="nav-link active">
                   Experience
                 </a>
               </li>
+              <div class="horizontal_gap"></div>
               <li class="nav-item active">
                 <a href="/portfolio/hobbies" class="nav-link active">
                   Hobbies
                 </a>
               </li>
+              <div class="horizontal_gap"></div>
               <li class="nav-item active">
                 <a href="/portfolio/contact" class="nav-link active">
                   Contact
