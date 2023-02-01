e35d257ef5133ea31b723aa091c81b1728998a22
Fe-56 <limfuoen1999@gmail.com>
2023-01-31 16:53:12 -0800

    Test react router with Github Pages #4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
@@ -11,35 +11,25 @@ import Hobbies from "./components/Hobbies/Hobbies";
 
 export default function App() {
   return (
-    <HashRouter basename="/">
-      <div id="app">
-        <Navbar />
-        {/* <BrowserRouter>
-          <Routes>
-            <Route path="/portfolio" element={<About />} />
-            <Route path="/portfolio/projects" element={<Projects />} />
-            <Route path="/portfolio/skills" element={<Skills />} />
-            <Route path="/portfolio/experiences" element={<Experiences />} />
-            <Route path="/portfolio/hobbies" element={<Hobbies />} />
-            <Route path="/portfolio/contact" element={<Contact />} />
-          </Routes>
-        </BrowserRouter> */}
+    <div id="app">
+      <Navbar />
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/experiences" element={<Experiences />} />
-          <Route path="/hobbies" element={<Hobbies />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<About />} />
+          <Route path="/portfolio/projects" element={<Projects />} />
+          <Route path="/portfolio/skills" element={<Skills />} />
+          <Route path="/portfolio/experiences" element={<Experiences />} />
+          <Route path="/portfolio/hobbies" element={<Hobbies />} />
+          <Route path="/portfolio/contact" element={<Contact />} />
         </Routes>
-        <div id="footer">
-          <a href="https://github.com/Fe-56/portfolio" target="_blank" id="github_source">
-            <i class="fa-brands fa-github fa-xl" id="github_icon"></i>
-            Github Source Code
-          </a>
-          <p>© 2023 Fuo En Lim. All Rights Reserved.</p>
-        </div>
+      </BrowserRouter>
+      <div id="footer">
+        <a href="https://github.com/Fe-56/portfolio" target="_blank" id="github_source">
+          <i class="fa-brands fa-github fa-xl" id="github_icon"></i>
+          Github Source Code
+        </a>
+        <p>© 2023 Fuo En Lim. All Rights Reserved.</p>
       </div>
-    </HashRouter>
+    </div>
   );
 }
